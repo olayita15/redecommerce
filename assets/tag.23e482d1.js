8a180d92d050963f0e25c9956889e15f10720cad
root <chrisolayadev@gmail.com>
2024-01-31 05:06:40 +0000

    Updates

--- assets/tag.23e482d1.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.527b72fb.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
---
+++ assets/tag.b8b6fcfb.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.24b8c8b4.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
