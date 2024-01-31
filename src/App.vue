597abf460e4a1cf7379dcbd6c0e6f38484bc23c5
olayita15 <chrisolayadev@gmail.com>
2024-01-31 10:05:10 -0500

    Se ajusta la responsividad de la home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
-    <router-view />
+    <div class="h-100 w-100 ma-0 pa-0 container">
+      <router-view />
+    </div>
   </v-app>
 </template>
 
-<script setup>
-  
-</script>
+<script setup></script>
 
 <style lang="scss">
 
