75f176bece02fa887e796bfab39ef275624db0ff
olayita15 <chrisolayadev@gmail.com>
2024-01-31 13:29:44 -0500

    Ajustes al diseño y la responsividad para mobiles pero ahora con un navBar diferente

---
+++ src/components/navigation/NavigationBar/navigationMobilBar.vue
@@ -0,0 +1,85 @@
+<template>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+<div class="navigation-mobile">
+      <v-app-bar  prominent style="background-color: #f4f4f4;" >
+        <v-app-bar-nav-icon
+          variant="text"
+          @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title
+          ><div class="logo secondary-title-font">
+            <span>TIENDA ROJA</span>
+          </div></v-toolbar-title
+        >
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        location="top"
+        temporary
+        class="primary-font"
+        style="background-color: #f4f4f4;">
+        <v-list :items="items"></v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+         <slot></slot> 
+      </v-main>
+    </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const drawer = ref(false);
+
+  const group = ref(null);
+  const items = ref( [
+      {
+          title: 'Ropa',
+          value: 'foo',
+      },
+      {
+          title: 'Libros',
+          value: 'bar',
+      },
+      {
+          title: 'Accesorios',
+          value: 'fizz',
+      },
+      {
+          title: 'Contacto',
+          value: 'buzz',
+      },
+  ]);
+</script>
+
+<style lang="scss" scoped>
+.color-nav{
+  background-color: #f4f4f4;
+
+}
+
+.navigation-mobile{
+    display: none;
+  }
+    .principal-box{
+        max-width: 100%;
+    }
+    @media (max-width: 900px) {
+      .navigation-desktop{
+        display: none;
+      }
+      .navigation-mobile{
+    display: flex;
+  }
+    }
+</style>
--- src/layouts/mainLayout.vue
+++ src/layouts/mainLayout.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="container">
-    <navigation-bar />
-    <v-container fluid class="pa-0 ma-0" grid-list-xs>
-      <v-container fluid class="pa-0 ma-0">
-        <router-view></router-view>
+    <navigation-bar class="navigation-desktop"/>
+    <navigationMobilBar >
+      <v-container fluid class="pa-0 ma-0" grid-list-xs>
+        <v-container fluid class="pa-0 ma-0">
+          <router-view></router-view>
+        </v-container>
       </v-container>
-    </v-container>
+    </navigationMobilBar>
+    <div class="navigation-desktop">
+      <v-container fluid class="pa-0 ma-0" grid-list-xs>
+        <v-container fluid class="pa-0 ma-0">
+          <router-view></router-view>
+        </v-container>
+      </v-container>
+    </div>
     <principal-footer />
   </div>
 </template>
 
 <script setup>
   import navigationBar from '@/components/navigation/NavigationBar/navigationBar.vue';
+  import navigationMobilBar from '../components/navigation/NavigationBar/navigationMobilBar.vue';
+
   import PrincipalFooter from '@/components/navigation/footers/principalFooter.vue';
 </script>
 
 <style lang="scss" scoped>
-  .principal-box{
-      max-width: 100%;
+  .navigation-mobile{
+    display: none;
   }
+    .principal-box{
+        max-width: 100%;
+    }
+    @media (max-width: 900px) {
+      .navigation-desktop{
+        display: none;
+      }
+      .navigation-mobile{
+    display: flex;
+  }
+    }
 </style>
