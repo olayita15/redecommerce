46631cb27f7db569af9a83e6f31eebd6df01aded
olayita15 <chrisolayadev@gmail.com>
2024-02-01 17:53:10 -0500

    Se crea un nav bar para mobil y se implementa para todo el sistema, se ajustan los diseños de la cartas

---
+++ src/components/generalCards/genericMobileCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container class="card-box-container">
+    <v-card :loading="loading" class="mx-auto card-box elevation-5">
+      <template v-slot:loader="{ isActive }">
+        <v-progress-linear
+          :active="isActive"
+          color="deep-purple"
+          height="4"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+      <v-row>
+        <v-col>
+          <v-card-item>
+            <div class="card-item-box">
+              <img
+                class="image"
+                :src="genericCardData.image || 'https://cdn.vuetifyjs.com/images/cards/cooking.png'"
+              />
+              <v-card-title class="max-width mt-3">
+                <div class="title-text primary-title-font">
+                  <span class="break-word">{{ genericCardData.title || 'Default Title' }}</span>
+                </div>
+              </v-card-title>
+              <v-card-subtitle class="max-width ma-1">
+                <span
+                  class="me-1 autor-font-regular"
+                  style="font-size: large;"
+                >{{ genericCardData.author || 'Default Subtitle' }}</span
+                >
+                <v-icon
+                  :color="genericCardData.iconColor || 'error'"
+                  :icon="genericCardData.icon || 'mdi-fire-circle'"
+                  size="small"
+                ></v-icon>
+              </v-card-subtitle>
+            </div>
+          </v-card-item>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const props = defineProps(['genericCardData']);
+
+  const loading = ref(false);
+
+  // ... Resto del código
+</script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: 900px) {
+  .card-item-box{
+    display: flex;
+    flex-flow: column nowrap;
+    word-wrap: break-word;
+  }
+
+  .image {
+    max-width: 70%; /* Ajusta el ancho máximo de la imagen según tus necesidades */
+    height: auto; /* Mantiene la proporción original de la imagen */
+    align-self: center;
+  }
+
+  .card-box {
+    width: 300px;
+    height: 340px;
+    border-radius: 25px;
+
+  }
+
+  .title-text {
+    font-size: 20px;
+    margin: 5px 0;
+    white-space: wrap; 
+    overflow: hidden;
+    text-overflow: ellipsis; 
+  }
+}
+</style>
--- src/components/navigation/NavigationBar/navigationMobilBar.vue
+++ src/components/navigation/NavigationBar/navigationMobilBar.vue
@@ -1,45 +1,87 @@
 <template>
-      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
-<div class="navigation-mobile">
-      <v-app-bar  prominent style="background-color: #f4f4f4;" >
-        <v-app-bar-nav-icon
-          variant="text"
-          @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+  <div class="navigation-mobile">
+    <v-app-bar
+      class="d-flex justify-center aling-center "
+      prominent
+      style="background-color: #f4f4f4;">
+      <v-app-bar-nav-icon
+        class="primary-color mobile-display"
+        variant="text"
+        @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-        <v-toolbar-title
-          ><div class="logo secondary-title-font">
-            <span>TIENDA ROJA</span>
-          </div></v-toolbar-title
-        >
+      <v-toolbar-title
+        ><div class="mt-1 main-nav secondary-title-font">
+          <a class="mobile-display">TR</a>
+          <a class="desktop-display" href="#">TIENDA ROJA</a>
+        </div></v-toolbar-title
+      >
+      <div class="desktop-display">
+        <ul class="main-nav secondary-title-font">
+          <li><a href="#">Ropa</a></li>
+          <li><a href="#">Libros</a></li>
+          <li><a href="#">Accesorios</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </div>
+      <v-btn
+        class="primary-color"
+        variant="text"
+        icon="mdi-magnify"
+        @click.stop="search = !search"></v-btn>
 
-        <v-spacer></v-spacer>
+      <v-btn
+        class="primary-color"
+        variant="text"
+        icon="mdi-shopping-outline"></v-btn>
 
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+      <!-- <v-btn
+        class="primary-color"
+        variant="text"
+        icon="mdi-dots-vertical"></v-btn> -->
+    </v-app-bar>
 
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
+    <v-navigation-drawer
+      v-model="drawer"
+      location="left"
+      temporary
+      class="primary-font"
+      style="background-color: #f4f4f4;">
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
 
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
+    <v-navigation-drawer
+      v-model="search"
+      location="top"
+      temporary
+      class="primary-font"
+      style="background-color: #f4f4f4;"
+      :width="100">
+      <div class="search-drawer">
+        <v-text-field
+          class="w-75 mt-7 primary-color"
+          style="justify-content: flex-end;"
+          clearable
+          label="Buscar por productos"
+          prepend-icon="mdi-magnify"
+          variant="underlined"
+          rounded
+          density="compact"></v-text-field>
+      </div>
+    </v-navigation-drawer>
 
-      <v-navigation-drawer
-        v-model="drawer"
-        location="top"
-        temporary
-        class="primary-font"
-        style="background-color: #f4f4f4;">
-        <v-list :items="items"></v-list>
-      </v-navigation-drawer>
-
-      <v-main>
-         <slot></slot> 
-      </v-main>
-    </div>
+    <v-main>
+      <slot></slot>
+    </v-main>
+  </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
 
   const drawer = ref(false);
+  const search = ref(false);
+
 
   const group = ref(null);
   const items = ref( [
@@ -63,23 +105,82 @@
 </script>
 
 <style lang="scss" scoped>
-.color-nav{
-  background-color: #f4f4f4;
+  .search-drawer{
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
 
-}
-
-.navigation-mobile{
-    display: none;
   }
-    .principal-box{
-        max-width: 100%;
-    }
-    @media (max-width: 900px) {
-      .navigation-desktop{
+  .primary-color{
+    color: #34495e;
+  }
+      .mobile-display{
         display: none;
       }
-      .navigation-mobile{
-    display: flex;
-  }
-    }
+
+          ul {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: center;
+        }
+
+        h2,
+        h3,
+        a {
+        	color: #34495e;
+        }
+
+        a {
+        	text-decoration: none;
+        }
+
+
+        .main-nav {
+        	margin-top: 5px;
+          font-size:x-large;
+        }
+        .logo a,
+        .main-nav a {
+        	padding: 10px 15px;
+        	text-align: center;
+        }
+        .main-nav a {
+        	color: #34495e;
+        	font-size: .99em;
+        }
+
+        .main-nav a:hover {
+        	color: #718daa;
+        }
+
+
+        /* =================================
+          Media Queries
+        ==================================== */
+    @media (max-width: 1050px) {
+      .mobile-display{
+        display: block;
+      }
+        .desktop-display{
+          display: none;
+        }
+        .logo{
+          text-align: center;
+        }
+
+        ul {
+          flex-flow: column wrap;
+        justify-content: center;
+
+        }
+        .cart-list{
+          display: none;
+        }
+        .cart-list-movil{
+          display: block;
+        }
+      }
 </style>
