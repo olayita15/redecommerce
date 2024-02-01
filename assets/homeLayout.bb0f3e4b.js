aab1e780ee1253d2eb67eec7d885aa1455a147c4
root <chrisolayadev@gmail.com>
2024-02-01 19:25:34 +0000

    Updates

---
+++ assets/homeLayout.bb0f3e4b.js
@@ -0,0 +1,1 @@
+import{_ as r,aj as s,ah as c,U as e,Q as n,e as a}from"./index.f22ba29f.js";import{V as o}from"./VContainer.723989d2.js";const _={};function i(f,l){const t=s("router-view");return n(),c(o,{fluid:"",class:"pa-0 ma-0","grid-list-xs":""},{default:e(()=>[a(o,{fluid:"",class:"pa-0 ma-0"},{default:e(()=>[a(t)]),_:1})]),_:1})}var d=r(_,[["render",i]]);export{d as default};
