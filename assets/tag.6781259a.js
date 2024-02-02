160b6e40b7bbd88cbce889e433704a4411f00a1f
root <chrisolayadev@gmail.com>
2024-02-02 20:12:16 +0000

    Updates

---
+++ assets/tag.6781259a.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.b94531aa.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
--- assets/tag.e03d15bb.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.20d16581.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
