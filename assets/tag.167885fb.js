87513bd5ac4e96524186848370aca7a376739cc9
root <chrisolayadev@gmail.com>
2024-02-02 17:50:42 +0000

    Updates

---
+++ assets/tag.167885fb.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.a9459e36.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
--- assets/tag.a16a6199.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.50982812.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
