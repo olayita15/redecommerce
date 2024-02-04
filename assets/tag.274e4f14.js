1b3df361beb71981774d27d2821084df1c21f083
root <chrisolayadev@gmail.com>
2024-02-04 18:23:35 +0000

    Updates

---
+++ assets/tag.274e4f14.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.4a67a01f.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
--- assets/tag.6781259a.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.b94531aa.js";const t=a({tag:{type:String,default:"div"}},"tag");export{t as m};
