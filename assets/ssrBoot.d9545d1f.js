baa4c2ee1d95dc709234ec8f04cdfcc5e0587105
root <chrisolayadev@gmail.com>
2024-05-14 05:20:05 +0000

    Updates

--- assets/ssrBoot.d9545d1f.js
+++
@@ -1,1 +0,0 @@
-import{s,z as t,a as e,aD as n}from"./index.13caf822.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
---
+++ assets/tag.598c265e.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index.506b1647.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
