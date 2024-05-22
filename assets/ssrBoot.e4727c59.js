59b1c5c46c96de8ad55f6e48f72c503e06d4c115
root <chrisolayadev@gmail.com>
2024-05-22 04:31:40 +0000

    Updates

--- assets/ssrBoot.e4727c59.js
+++
@@ -1,1 +0,0 @@
-import{s as e,o as s,e as t,az as n}from"./index.709dc84d.js";function i(){const o=e(!1);return s(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:t(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
---
+++ assets/tag.481c7bee.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index.9b385d63.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
